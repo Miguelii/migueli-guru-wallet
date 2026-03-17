1aeceb6fd99c4d15c83629c525eb9fc155eaadaf
Miguel Gonçalves <miguelgoncalves18@hotmail.com>
2026-03-17 23:30:15 +0000

    update pnpm

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import { useTheme } from 'next-themes'
 import { Toaster as Sonner, type ToasterProps } from 'sonner'
 import {
     CircleCheckIcon,
@@ -11,7 +10,7 @@ import {
 } from 'lucide-react'
 
 const Toaster = ({ ...props }: ToasterProps) => {
-    const { theme = 'system' } = useTheme()
+    const theme = 'system'
 
     return (
         <Sonner
